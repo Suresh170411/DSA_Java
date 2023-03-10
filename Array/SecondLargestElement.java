0ea7f66197323a20e610dbe4e190a514900874a6
suresh170411 <suresh.20fk@gmail.com>
2023-03-10 21:13:35 +0530

    DSA : Find the second largest element in array

--- Array/SecondLargestElement.java
+++ Array/SecondLargestElement.java
@@ -6,10 +6,10 @@ public class SecondLargestElement {
     public static void main(String[] args) {
         int [] arr = {3,6,1,6,2,0,8,9};
 
-        System.out.println(findSecondLargestElement(arr));
-        secondLargest(arr);
+        System.out.println(findSecondLargest(arr));
     }
 
+    // brute force approach
     public static int findSecondLargestElement(int arr []){
         int max = Integer.MIN_VALUE;
 
@@ -34,5 +34,20 @@ public class SecondLargestElement {
     public static void secondLargest(int [] arr){
         Arrays.sort(arr);
         System.out.println(arr[arr.length-2]);
+    }
+
+    public static int findSecondLargest(int [] arr){
+        int max = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] > max){
+                secondLargest = max;
+                max = arr[i];
+            }else if (arr[i] > secondLargest && arr[i] != max){
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
     }
 }
