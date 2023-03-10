a63bd6c3802563e8eca60be40e82ae2bce30d934
suresh170411 <suresh.20fk@gmail.com>
2023-03-10 21:01:25 +0530

    DSA : Find min and max from an array java

---
+++ Array/FindMinAndMaxValue.java
@@ -0,0 +1,32 @@
+package Array;
+
+public class FindMinAndMaxValue {
+    public static void main(String[] args) {
+        int [] arr = {2,5,15,7,9,0,1};
+
+        System.out.println(findMin(arr));
+        System.out.println(findMax(arr));
+    }
+
+    public static int findMin(int arr []){
+        int min = arr[0];
+
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static int findMax(int arr []){
+        int max = arr[0];
+
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
