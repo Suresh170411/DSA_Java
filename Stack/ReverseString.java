8784a2cdba641fc1f74a0d88401401f39a198538
suresh170411 <suresh.20fk@gmail.com>
2023-03-11 18:31:12 +0530

    DSA : Reverse String using Stack

---
+++ Stack/ReverseString.java
@@ -0,0 +1,24 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(reverseString(str));
+    }
+
+    public static String reverseString(String str){
+        Stack<Character> stack = new Stack<>();
+
+        char [] arr = str.toCharArray();
+
+        for (char c : arr){
+            stack.push(c);
+        }
+        for (int i=0; i<str.length(); i++){
+            arr[i] = stack.pop();
+        }
+        return new String(arr);
+    }
+}
