bb72ab10eff106bbad30ca7c52c472a027d330ae
suresh170411 <suresh.20fk@gmail.com>
2023-03-12 23:27:59 +0530

    DSA : reduce string bruteforce approach adn stack solution

--- Stack/ReduceString.java
+++ Stack/ReduceString.java
@@ -4,25 +4,56 @@ import java.util.Stack;
 
 public class ReduceString {
     public static void main(String[] args) {
-        String str = "aabbcc";
+        String str = "aaaaaaaa";
         reduceString(str);
+        String res = bruteForceApproach(str);
+        System.out.println(res);
     }
     public static void reduceString(String str){
         Stack<Character> st = new Stack<>();
 
-        char arr [] = str.toCharArray();
+        for (int i=0; i<str.length(); i++){
+            char x = str.charAt(i);
 
-        for (int i=0; i<arr.length; i++){
-            if (!st.isEmpty() && st.peek() == arr[i]){
+            if (!st.isEmpty() && st.peek() == x){
                 st.pop();
             }else {
-                st.push(arr[i]);
+                st.push(x);
             }
         }
+        StringBuilder sb = new StringBuilder();
+
+        for (Character c : st){
+            sb.append(c);
+        }
+
         if (st.isEmpty()){
-            System.out.println("Empty String");
+            System.out.println("Empty!");
         }else {
-            System.out.println(st);
+            System.out.println(sb.toString());
+        }
+    }
+
+    // reduce String brure force approach
+    public static String bruteForceApproach(String str){
+
+        StringBuilder sb = new StringBuilder(str);
+        
+        boolean havePair = true;
+        while (havePair){
+            havePair = false;
+            for (int i=0; i<sb.length()-1; i++){
+                if (sb.charAt(i) == sb.charAt(i+1)){
+                    sb.delete(i, i+2);
+                    havePair = true;
+                    break;
+                }
+            }
+        }
+        if (sb.length() == 0){
+            return "EMPTY";
+        }else {
+            return sb.toString();
         }
     }
 }
