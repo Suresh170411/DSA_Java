3ce2dbb770ba9b3fcb6a1e59d5737dd6efe08e19
suresh170411 <suresh.20fk@gmail.com>
2023-03-12 19:24:53 +0530

    DSA : find next Greater elment in the array

--- Stack/NextGreaterElement.java
+++ Stack/NextGreaterElement.java
@@ -1,23 +1,30 @@
 package Stack;
 
+import java.util.Arrays;
+
 public class NextGreaterElement {
     public static void main(String[] args) {
         int arr [] = {10,2,4,3,8,9,1};
-        nextGreaterBF(arr);
+        System.out.println(Arrays.toString(nextGreaterBF(arr)));
     }
 
     // brute force approach for finding next greater element
-    public static void nextGreaterBF(int [] arr){
-        for (int i=0; i<arr.length; i++){
+    public static int [] nextGreaterBF(int [] arr){
+        int n = arr.length; 
+
+        int result [] = new int [n];
+
+        for (int i=0; i<n; i++){
             int res = -1;
-            for (int j=i+1; j<arr.length; j++){
+            for (int j=i+1; j<n; j++){
                 if (arr[i] < arr[j]){
                     res = arr[j];
                     break;
                 }
             }
-            System.out.print(res+" ");
+            result[i] = res;
         }
+        return result;
     }
 
 
