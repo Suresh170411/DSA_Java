89245fdba87f71bc030edb6c2deec8ee5c9d6024
suresh170411 <suresh.20fk@gmail.com>
2023-03-14 23:37:24 +0530

    DSA : STACK nearest smaller element to the right

---
+++ Stack/NearestSmallerRight.java
@@ -0,0 +1,33 @@
+package Stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NearestSmallerRight {
+    public static void main(String[] args) {
+        int arr [] = {7,2,4,6,9,3,8,11};
+        nextGreaterRight(arr);
+    }
+    
+    public static void nextGreaterRight(int [] arr){
+        int n = arr.length;
+        int res [] = new int [n];
+        Stack<Integer> st = new Stack<>();
+
+        res[n-1] = -1;
+        st.push(n-1);
+        
+        for (int i=n-2; i>=0; i--){
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]){
+                st.pop();
+            }
+            if (!st.isEmpty()){
+                res[i] = arr[st.peek()];
+            }else {
+                res[i] = -1;
+            }
+            st.push(i);
+        }
+        System.out.println(Arrays.toString(res));
+    }
+}
