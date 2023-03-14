9530be83f119726083f6e84442d087fd63010675
suresh170411 <suresh.20fk@gmail.com>
2023-03-14 20:06:23 +0530

    DSA : Stock Span Problem

--- Stack/PreviousGreaterElement.java
+++ Stack/PreviousGreaterElement.java
@@ -1,12 +1,37 @@
 package Stack;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class PreviousGreaterElement {
     public static void main(String[] args) {
         int arr [] = {2,5,1,7,6};
         previousGreaterElementBF(arr);
+        System.out.println(Arrays.toString(nextGreaterElement(arr)));
     }
+
+    public static int [] nextGreaterElement(int arr []){
+        
+        int n = arr.length;
+
+        int res [] = new int [n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i=0; i<n; i++){
+            if (!st.isEmpty()){
+                while(!st.isEmpty() && st.peek() >= arr[i]){
+                    st.pop();
+                }
+            }
+            if (st.isEmpty()){
+                res[i] = -1;
+            }else {
+                res[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        return res;
+    }
+
     public static void previousGreaterElementBF(int arr []){
         int n = arr.length;
         int [] result = new int [n];
--- Stack/StockSpan.java
+++ Stack/StockSpan.java
@@ -1,15 +1,35 @@
 package Stack;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class StockSpan {
     public static void main(String[] args) {
         int arr [] = {100,80,60,70,60,75,85};
         bruteForceApproach(arr);
+        stockSpanPrint(arr);
     }
 
     public static void stockSpanPrint(int arr []){
-        
+        int n = arr.length;
+        int [] res = new int [n];
+        Stack<Integer> st = new Stack<>();
+
+        res[0] = 1;
+        st.push(0);
+
+        for (int i=1; i<n; i++){
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+                st.pop();
+            }
+            if (!st.isEmpty()){
+                res[i] = i-st.peek();
+            }else {
+                res[i] = i+1;
+            }
+            st.push(i);
+        }
+        System.out.println(Arrays.toString(res));
     }
 
     public static void bruteForceApproach(int arr []){
