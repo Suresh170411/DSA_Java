3a4d723e6506c536442993f58c17caa57064c211
suresh170411 <suresh.20fk@gmail.com>
2023-03-14 23:30:46 +0530

    DSA : STACK nearest smaller element to the left

--- Stack/NearestSmallerElementLeft.java
+++ Stack/NearestSmallerElementLeft.java
@@ -7,6 +7,7 @@ public class NearestSmallerElementLeft {
     public static void main(String[] args) {
         int arr [] = {7,2,4,6,9,3,8,11};  // --> [-1, -1, 2, 4, 6, 2, 3, 8] answer output
         nearestSmaller(arr);
+        bruteForceApproach(arr);
     }
     public static void nearestSmaller(int arr []){
         int n = arr.length;
@@ -24,5 +25,22 @@ public class NearestSmallerElementLeft {
             st.push(i);
         }
         System.out.println(Arrays.toString(res));
+    }
+
+    public static void bruteForceApproach(int arr []){
+        int n = arr.length;
+        int ans [] = new int [n];
+
+        for (int i=n-1; i>=0; i--){
+            int res = -1;
+            for (int j=i-1; j>=0; j--){
+                if (arr[i] > arr[j]){
+                    res = arr[j];
+                    break;
+                }
+            }
+            ans[i] = res;
+        }
+        System.out.println(Arrays.toString(ans));
     }
 }
