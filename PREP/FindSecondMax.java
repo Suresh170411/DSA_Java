be5ca52fcd0ae4e42a02f4e0d80ec906fcb39c7b
suresh170411 <suresh.20fk@gmail.com>
2023-03-23 22:14:37 +0530

    DSA : Find The Second Largest Element

--- PREP/FindSecondMax.java
+++ PREP/FindSecondMax.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+import java.util.TreeSet;
+
 public class FindSecondMax {
     public static void main(String[] args) {
         int [] arr = {3,5,8,8,1,7,0,2,7};
         System.out.println(getSecondMax(arr));
+        printSecondMaxElement(arr);
     }
     public static int getSecondMax(int arr []){
         int n = arr.length;
@@ -20,5 +24,15 @@ public class FindSecondMax {
             }
         }
         return secondMax;
+    }
+
+    public static void printSecondMaxElement(int arr []){
+        TreeSet<Integer> ts = new TreeSet<>();
+        for (int i : arr){
+            ts.add(i);
+        }
+        ts.remove(ts.last());
+
+        System.out.println("Second Largest Element is : " + ts.last());
     }
 }
