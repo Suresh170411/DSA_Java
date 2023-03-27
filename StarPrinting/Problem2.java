8ce8cff60388cc523e82fc8720a9dac05dfe22c0
suresh170411 <suresh.20fk@gmail.com>
2023-03-27 22:16:40 +0530

    Star Pattern printing hollow practice

---
+++ StarPrinting/Problem2.java
@@ -0,0 +1,14 @@
+package StarPrinting;
+
+/**
+ *      1   2   3   4
+ *      5           8
+ *      9           12
+ *      13  14  15  16
+ */
+
+public class Problem2 {
+    public static void main(String[] args) {
+        
+    }
+}
