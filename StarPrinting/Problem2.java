57727782d59f787a18444e2b0e8a58e012b6f61e
suresh170411 <suresh.20fk@gmail.com>
2023-03-28 07:57:48 +0530

    Start pattern hollow square with numbers

--- StarPrinting/Problem2.java
+++ StarPrinting/Problem2.java
@@ -10,6 +10,7 @@ package StarPrinting;
 public class Problem2 {
     public static void main(String[] args) {
         printStars(4);
+        printNums(4);
     }
     public static void printStars(int size){
 
@@ -17,6 +18,22 @@ public class Problem2 {
             for (int j=0; j<size; j++){
                 if (i==0 || i==size-1 || j==0 || j==size-1){
                     System.out.print(" * ");
+                }else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printNums(int size){
+        int count = 0;
+
+        for (int i=0; i<size; i++){
+            for (int j=0; j<size; j++){
+                count++;
+                if (i==0 || i==size-1 || j==0 || j==size-1){
+                    System.out.print(" "+count+" ");
                 }else {
                     System.out.print("   ");
                 }
