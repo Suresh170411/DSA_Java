4a1db05e439c570cfbb076af348c5a1db2a65842
suresh170411 <suresh.20fk@gmail.com>
2023-03-30 23:36:01 +0530

    DSA : STACK nearest smaller element to the left

---
+++ Stack/SmallerElementToLeft.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class SmallerElementToLeft {
+    public static void main(String[] args) {
+        int arr [] = {1,6,4,10,2,5};
+        optimizedApproach(arr);
+    }
+    public static void optimizedApproach(int arr []){
+        int n = arr.length;
+        int res [] = new int [n];
+        Stack<Integer> st = new Stack<>();
+
+        res[0] = -1;
+        st.push(0);
+
+        for (int i=1; i<n; i++){
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]){
+                st.pop();
+            }
+            res[i] = !st.isEmpty() ? arr[st.peek()] : -1;
+            st.push(i);
+        }
+        System.out.println(Arrays.toString(res));
+     }
+}
