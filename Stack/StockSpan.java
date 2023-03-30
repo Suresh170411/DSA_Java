81d95795d6c24cb19627bb7452a0b717f30b6cbd
suresh170411 <suresh.20fk@gmail.com>
2023-03-30 23:09:58 +0530

    DSA : STACK stock span problem

--- Stack/StockSpan.java
+++ Stack/StockSpan.java
@@ -1,5 +1,25 @@
 // package Stack;
 
+/**
+  The stock span problem is a financial problem where we have a series of N daily price quotes 
+  for a stock and we need to calculate the span of the stock’s price for all N days. 
+  The span Si of the stock’s price on a given day i is defined as the maximum number of 
+  consecutive days just before the given day, for which the price of the stock on the current 
+  day is less than its price on the given day. 
+
+Examples:
+
+Input: N = 7, price[] = [100 80 60 70 60 75 85]
+Output: 1 1 1 2 1 4 6
+
+    Explanation: Traversing the given input span for 100 will be 1, 
+    80 is smaller than 100 so the span is 1, 
+    60 is smaller than 80 so the span is 1, 
+    70 is greater than 60 so the span is 2 and so on. 
+    Hence the output will be 1 1 1 2 1 4 6.
+ */
+
+
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -60,16 +80,12 @@ public class StockSpan {
         st.push(0);
 
         for (int i=1; i<arr.length; i++){
-            int x = arr[i];
-
-            while (!st.isEmpty() && arr[st.peek()] <= x){
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]){
                 st.pop();
             }
-            res[i] = st.isEmpty() ?  i+1 : i-st.peek();
-
+            res[i] = st.isEmpty() ? i+1 : i-st.peek();
             st.push(i);
         }
-
         System.out.println(Arrays.toString(res));
     }
 }
