f7349c39fb4289b8acb6b8a59c8dc5e326eed330
suresh170411 <suresh.20fk@gmail.com>
2023-03-31 19:23:48 +0530

    DSA : STACK Next Greater Element to Right problem

---
+++ Stack/GeraterElementToRight.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class GeraterElementToRight {
+    public static void main(String[] args) {
+        int arr [] = {1,6,4,10,2,5};
+        ngr(arr);
+    }
+
+    public static void ngr(int arr []){
+        int n = arr.length;
+        int res [] = new int [n];
+        Stack<Integer> st = new Stack<>();
+
+        res[n-1] = -1;
+        st.push(n-1);
+
+        for (int i=n-2; i>=0; i--){
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]){
+                st.pop();
+            }
+            res[i] = !st.isEmpty() ? arr[st.peek()] : -1;
+            st.push(i);
+        }
+        System.out.println(Arrays.toString(res));
+    }
+}
