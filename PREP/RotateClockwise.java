39d68bda20c34f0144d0e5f7a7562c200d12ef90
suresh170411 <suresh.20fk@gmail.com>
2023-04-01 21:18:26 +0530

    DSA : Rotate 2D matrix by 90 degree clockwise

--- PREP/RotateClockwise.java
+++ PREP/RotateClockwise.java
@@ -37,4 +37,24 @@ public class RotateClockwise {
             System.out.println(bag);
         }
     }
+
+    public static void optimizedApproach(int[][]arr,int n){
+        
+        for (int i=0; i<n/2; i++){
+            for (int j=i; j<n-i-1; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[n-j-1][i];
+                arr[n-j-1][i] = arr[n-i-1][n-j-1];
+                arr[n-i-1][n-j-1] = arr[j][n-i-1];
+                arr[j][n-i-1] = temp;
+            }
+        }
+        
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
