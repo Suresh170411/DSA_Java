8cb868c08ead508e9ec2294d0125b2b5ffa9114a
suresh170411 <suresh.20fk@gmail.com>
2023-04-05 22:56:59 +0530

    DSA : Recursion print numbers in reverse basic prolem

--- Recursion/PrintNumbers.java
+++ Recursion/PrintNumbers.java
@@ -1,3 +1,13 @@
+
+/**
+ * Print from 5 to 1 without using any loop
+ * 
+ * Hint : Use Recursion
+ * 
+ * Input : 5 to 1
+ * Output : 5 4 3 2 1
+ */
+
 public class PrintNumbers {
     public static void main(String[] args) {
         int n = 5;
