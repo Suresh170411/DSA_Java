a6405fe7aa78c0eeb8fc6fca6d1850b1bb2f75e0
suresh170411 <suresh.20fk@gmail.com>
2023-04-19 09:48:29 +0530

    DSA : String first letter uppers case basic java problem practice

---
+++ PREP/StringFirstUpperCase.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class StringFirstUpperCase {
+    public static void main(String[] args) {
+        String str = "hello there how are you";
+
+        String arr [] = str.split("\\s");
+
+        System.out.println(Arrays.toString(arr));
+
+        String bag = "";
+        for (int i=0; i<arr.length; i++){
+            String first = arr[i].substring(0, 1);
+            String last = arr[i].substring(1);
+
+            bag+= first.toUpperCase()+last + " ";
+        }
+
+        System.out.println(bag);
+    }
+}
