840e6f00317b18d958df8964a388dc28d792128c
suresh170411 <suresh.20fk@gmail.com>
2023-04-21 21:18:34 +0530

    DSA : LinkedList practice

---
+++ LinkedList/LinkedList1.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    void printNode(){
+        System.out.println(this.data+" -> "+this.next);
+    }
+}
+
+
+
+public class LinkedList1 {
+    
+    Node head;
+
+    public LinkedList1(){
+        this.head = null;
+    }
+
+    // function to add element to the last/tail of the linkedlist
+    public void insertAtTail(int data){
+        
+    }
+}
