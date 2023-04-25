7369a85fbf3ee20b22d22593be623472506d3c0a
suresh170411 <suresh.20fk@gmail.com>
2023-04-25 17:52:13 +0530

    Stream API practice java

--- StreamAPI/FilterMethod.java
+++ StreamAPI/FilterMethod.java
@@ -2,7 +2,6 @@ package StreamAPI;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 // intermediate method
 public class FilterMethod {
@@ -15,13 +14,25 @@ public class FilterMethod {
         students.add(new Student(40, "Pradeep", 450));
         students.add(new Student(50, "Chiku", 520));
 
+        /** 
         Stream<Student> stream = students.stream();
         Stream<Student> filtered = stream.filter(s -> s.getMarks() > 500);
+
         filtered.forEach(s -> {
             System.out.println("ROLL : " + s.getRoll());
             System.out.println("NAME : " + s.getName());
             System.out.println("MARK : " + s.getMarks());
             System.out.println("=============");
         });
+        */
+
+        students.stream().filter(s->s.getMarks() > 500).forEach(s->{
+            System.out.println("ROLL : " + s.getRoll());
+            System.out.println("NAME : " + s.getName());
+            System.out.println("MARK : " + s.getMarks());
+            System.out.println("=============");
+        });
+
+        
     }
 }
