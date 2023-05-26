005e548d0dffe0a923cd63f6103f1181725c5615
suresh170411 <suresh.20fk@gmail.com>
2023-05-26 13:53:41 +0530

    Star pattern problems java

--- StarPrinting/Problem1.java
+++ StarPrinting/Problem1.java
@@ -8,6 +8,13 @@ public class Problem1 {
         printNums(5, 4);
         
     }
+    /**
+     * The function prints a rectangular pattern of stars with the specified number of rows and
+     * columns.
+     * 
+     * @param row The number of rows in the pattern of stars to be printed.
+     * @param column The number of columns in the grid of stars that will be printed.
+     */
     public static void printStars(int row, int column){
         for (int i=1; i<=row; i++){
             for (int j=1; j<=column; j++){
@@ -17,6 +24,13 @@ public class Problem1 {
         }
     }
 
+    /**
+     * The function prints a grid of characters from 'a' to 'z' with the specified number of rows and
+     * columns.
+     * 
+     * @param row The number of rows to be printed.
+     * @param column The number of characters to be printed in each row.
+     */
     public static void printChars(int row, int column){
         String str = "abcdefghijklmnopqrstuvwxyz";
         int count = 0;
@@ -29,6 +43,13 @@ public class Problem1 {
         }
     }
 
+    /**
+     * The function prints a matrix of numbers from 1 to row*column.
+     * 
+     * @param row The number of rows to be printed.
+     * @param column The parameter "column" represents the number of columns in the grid that will be
+     * printed by the method "printNums".
+     */
     public static void printNums(int row, int column){
         int count = 0;
         for (int i=1; i<=row; i++){
