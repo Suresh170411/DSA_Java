54590b75c5cf102663d0c466c9517698a14e2f86
suresh170411 <suresh.20fk@gmail.com>
2023-05-27 20:54:51 +0530

    List sort using external implemented class inner class and LE

--- Collection/ArrayList/SortUsingComparator.java
+++ Collection/ArrayList/SortUsingComparator.java
@@ -2,6 +2,7 @@ package Arraylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import StreamAPI.Student;
@@ -14,7 +15,29 @@ public class SortUsingComparator {
         students.add(new Student(20, "Saumya", 520));
         students.add(new Student(30, "Priti", 590));
 
-        Collections.sort(students, new ListSortComparator());
+        /**
+         * sort students using external implemented class
+         */
+        // Collections.sort(students, new ListSortComparator());  
+
+
+        /**
+         * sort students using inner class implementation
+         */
+        // Collections.sort(students, new Comparator<Student>() {
+
+        //     @Override
+        //     public int compare(Student o1, Student o2) {
+        //         return o1.getMarks() > o2.getMarks() ? +1 : -1;
+        //     }
+            
+        // });
+
+
+        /**
+         * sort students using Lambda Expression
+         */
+        Collections.sort(students, (s1,s2)-> s1.getMarks() > s2.getMarks() ? +1 : -1);
 
         for (Student s : students){
             System.out.println(s.getRoll()+" : "+s.getName()+" : "+s.getMarks());
