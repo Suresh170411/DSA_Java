435cffa11bd8ee8acace42658b7ba53b98cd21d2
suresh170411 <suresh.20fk@gmail.com>
2023-06-12 10:56:09 +0530

    DSA : Factorial printing practice problem

--- Basics/Factorial.java
+++ Basics/Factorial.java
@@ -1,8 +1,9 @@
 public class Factorial {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 5;
 
         System.out.println(findFactorial(n));
+        System.out.println(printFact(n));
     }
 
     public static int findFactorial(int n){
@@ -11,5 +12,20 @@ public class Factorial {
         }
 
         return n * findFactorial(n-1);
+    }
+
+    public static int printFact(int n){
+        
+        if (n == 0){
+            return 0;
+        }
+
+        int res = 1;
+
+        for (int i=1; i<=n; i++){
+            res = res*i;
+        }
+
+        return res;
     }
 }
