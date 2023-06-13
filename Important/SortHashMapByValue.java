50b44e160fee25c6bf09ad282f031e1dad4f4d9d
suresh170411 <suresh.20fk@gmail.com>
2023-06-13 19:25:35 +0530

    Hashmap sorting by value java practice

---
+++ Important/SortHashMapByValue.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+public class SortHashMapByValue {
+    public static void main(String[] args) {
+        
+        HashMap<String,Student> hm = new HashMap<>();
+
+        hm.put("Odisha", new Student(10, "Suresh", 540));
+        hm.put("Kolkata", new Student(20, "Abhishek", 590));
+        hm.put("Mumbai", new Student(30, "Padma", 530));
+
+        sorthashMapUsingValue(hm);
+    }
+
+    public static void sorthashMapUsingValue(HashMap<String,Student> hm){
+        List<Entry<String,Student>> list = new ArrayList<>(hm.entrySet());
+
+        Collections.sort(list,(l1,l2)-> l1.getValue().getMarks() > l2.getValue().getMarks() ? 1 : -1);
+
+        for (Entry<String,Student> s : list){
+            System.out.println(s.getKey());
+            System.out.println("----------------");
+
+            Student st = s.getValue();
+            System.out.println("Roll : " + st.getRoll());
+            System.out.println("Name : " + st.getName());
+            System.out.println("Marks : " + st.getMarks());
+
+            System.out.println("================");
+        }
+    }
+}
