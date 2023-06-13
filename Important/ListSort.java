1b8cd6bee8d2480b09fdc7d4ca939da99b7199b7
suresh170411 <suresh.20fk@gmail.com>
2023-06-13 19:38:14 +0530

    List sort using LE for user defined class java practice problem

--- Important/ListSort.java
+++ Important/ListSort.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class ListSort {
     public static void main(String[] args) {
-        
+
         List<Integer> list = Arrays.asList(2,4,5,1,8,5,9);
         sortList(list);
 
@@ -21,16 +21,22 @@ public class ListSort {
     public static void sortList(List<Integer> list){
         Collections.sort(list);
 
+        System.out.print("Sorted List is : ");
         for (Integer i : list){
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println("\n");
     }
 
     public static void sortListCustomClass(List<Student> list){
         Collections.sort(list, (s1,s2)-> s1.getMarks() > s2.getMarks() ? 1 : -1);
 
         for (Student s : list){
-            System.out.println(s);
+            System.out.println("Name : " + s.getName());
+            System.out.println("Roll : " + s.getRoll());
+            System.out.println("Marks : " + s.getMarks());
+
+            System.out.println("-------------------");
         }
     }
 }
