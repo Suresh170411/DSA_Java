8310e46bdd088f417d0192089c0c8b33752fe7bf
suresh170411 <suresh.20fk@gmail.com>
2023-06-18 20:47:24 +0530

    String reverse using Collections Framework

--- String/StrReverseDot.java
+++ String/StrReverseDot.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class StrReverseDot {
     public static void main(String[] args) {
         
         String str = "hello.there.how.are.you";
         stringReverseWithDots(str);
 
+        reverseStrWithCollection(str);
 
         String words = "hello there how are you";
         reverseStrWithSpace(words);
@@ -49,5 +54,23 @@ public class StrReverseDot {
 
     public static void parseIntCheck(String num){
         System.out.println(Integer.parseInt(num));
+    }
+
+    public static void reverseStrWithCollection(String str){
+        String arr [] = str.split("[.]",0);
+
+        List<String> list = new ArrayList<>();
+
+        for (String s : arr){
+            list.add(s);
+        }
+
+        Collections.reverse(list);
+
+        System.out.print("From Collection : ");
+        for (String s : list){
+            System.out.print(s+" ");
+        }
+        System.out.println();
     }
 }
