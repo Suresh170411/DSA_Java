dda14877fceb68e474d69f2c69d6e0ca74b4a9ba
suresh170411 <suresh.20fk@gmail.com>
2023-06-19 22:58:29 +0530

    Switch case using optimal approach java

--- Basics/SwitchCase.java
+++ Basics/SwitchCase.java
@@ -7,6 +7,7 @@ public class SwitchCase {
         String day = "Friday";
         checkWithDays(day);
         switchCaseUsingLE(day);
+        switchCaseInOptimalWay1(day);
         
     }
 
@@ -50,8 +51,9 @@ public class SwitchCase {
 
     public static void switchCaseInOptimalWay1(String day){
 
-        String result = "";
+        String result = "Please wake before ";
 
+        /**
         switch(day){
             case "Monday","Tuesday","Wednessday","Thursday","Friday","Saturday" -> result = "Wake up at 7 AM";
 
@@ -59,6 +61,15 @@ public class SwitchCase {
 
             default -> result = "Take your time but before 8 AM";
         }
+         */
+        
+        result = switch(day){
+            case "Monday","Tuesday","Wednessday","Thursday","Friday","Saturday" -> result+= "6 AM";
+
+            case "Sunday" -> result+= "10 AM";
+
+            default -> result+= "8 AM";
+        };
 
         System.out.println(result);
     }
