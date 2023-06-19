7cc93460a555739de5ac7904e0a968f7e7b80c07
suresh170411 <suresh.20fk@gmail.com>
2023-06-19 22:34:27 +0530

    Switch case practice java

--- Basics/SwitchCase.java
+++ Basics/SwitchCase.java
@@ -1,5 +1,19 @@
 public class SwitchCase {
     public static void main(String[] args) {
         
+        int number = 6;
+        switch(number){
+            case 1 : System.out.println("This is One");
+            break;
+            case 2 : System.out.println("This is Two");
+            break;
+            case 3 : System.out.println("This is Three");
+            break;
+            case 4 : System.out.println("This is Four");
+            break;
+            case 5 : System.out.println("This is Five");
+            break;
+            default : System.out.println("Please select a number between 1 to 5 !");
+        }
     }
 }
