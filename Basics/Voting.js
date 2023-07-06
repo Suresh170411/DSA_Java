328c92c1f188684d67c455a198e2f19221ff173d
suresh170411 <suresh.20fk@gmail.com>
2023-07-06 19:26:30 +0530

    js file to check voting eligibility

---
+++ Basics/Voting.js
@@ -0,0 +1,12 @@
+function check(age){
+    
+    if (age >= 18){
+        console.log("Yes you are eligible");
+    }else {
+        console.log("No you are not eligible!")
+    }
+}
+
+let age = 18;
+
+check(age);
