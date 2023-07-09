aaa1bd8644937cd4d0b3b9f66e57160527a21e85
suresh170411 <suresh.20fk@gmail.com>
2023-07-09 16:14:15 +0530

    Regex pattern matcher practice java

--- PatternMatcher/PatternMatch.java
+++ PatternMatcher/PatternMatch.java
@@ -6,14 +6,29 @@ import java.util.regex.Pattern;
 public class PatternMatch{
     public static void main(String[] args) {
         
-        Pattern pattern = Pattern.compile("hi there"); 
-
-        Matcher matcher = pattern.matcher("Hi There");
+        // Pattern pattern = Pattern.compile("hi there");
+        // Pattern pattern = Pattern.compile("[aeiou]");
+        Pattern pattern = Pattern.compile("[0-9]");
+        Matcher matcher = pattern.matcher("1");
 
         if (matcher.matches()){
             System.out.println("Matches");
         }else {
             System.out.println("Invalid input");
+        }
+
+        checkWithWords();
+    }
+
+    public static void checkWithWords(){
+        String text = "Hi there how are you";
+
+        //+ sign is used when we check whole sentence or words
+        Pattern pattern = Pattern.compile("[a-z]+");
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()){
+            System.out.println(text.substring(matcher.start(), matcher.end()));
         }
     }
 }
