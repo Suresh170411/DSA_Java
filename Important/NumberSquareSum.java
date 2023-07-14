1087fe97941bdc652b27962f63f8329ca6f098df
suresh170411 <suresh.20fk@gmail.com>
2023-07-14 15:32:16 +0530

    update dsa practice

--- Important/NumberSquareSum.java
+++ Important/NumberSquareSum.java
@@ -7,6 +7,12 @@ public class NumberSquareSum {
     // 19 --> 1 - 9
     // 1^2 + 9^2 = 82
 
+    /**
+     * The function calculates the sum of the squares of the digits of a given number.
+     * 
+     * @param n The parameter "n" in the method "numberSquareSum" represents an integer number.
+     * @return The method is returning the sum of the squares of the digits of the given number.
+     */
     public static int numberSquareSum(int n){
         int sum = 0;
         while (n > 0){
