d9a6878e9483e2a688cabe4982c72a45ef986024
suresh170411 <suresh.20fk@gmail.com>
2023-07-16 21:22:16 +0530

    star pattern print dsa

--- StarPrinting/Problem2.java
+++ StarPrinting/Problem2.java
@@ -12,6 +12,14 @@ public class Problem2 {
         printStars(4);
         printNums(4);
     }
+    
+    /**
+     * The function prints a square of stars with a given size, where the border is filled with stars
+     * and the inside is empty.
+     * 
+     * @param size The size parameter determines the size of the square that will be printed. It
+     * represents the number of rows and columns in the square.
+     */
     public static void printStars(int size){
 
         for (int i=0; i<size; i++){
