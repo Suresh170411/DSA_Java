1b735cc8a2b9c8c474c102638a41acab2a8c1364
suresh170411 <suresh.20fk@gmail.com>
2023-07-23 22:23:04 +0530

    Consumer java practice

--- LambdaExpression/MyConsumer.java
+++ LambdaExpression/MyConsumer.java
@@ -1,5 +1,9 @@
 import java.util.function.Consumer;
 
+/**
+ * The MyConsumer class is a Java implementation of the Consumer interface that prints the roll, name,
+ * and marks of a Student object.
+ */
 public class MyConsumer implements Consumer<Student> {
 
     @Override
